247d5ab2ea05e1326fb62b67e97272fb4fb6118f
Mustafa-Siddique <66897095+Mustafa-Siddique@users.noreply.github.com>
2022-05-14 12:00:32 +0530

    comment-fixed

--- src/component/TaxationExplained.js
+++ src/component/TaxationExplained.js
@@ -8,20 +8,22 @@ export default function TaxationExplained() {
             <p className='text-light'>Buy- 4% Marketing Wallet, 1% Liquidity Pool, 1% Reflection and 1% Insurance</p>
             <p className='text-light'>Sell- 4% Marketing Wallet, 1% Liquidity Pool, 4% Reflection and 1% Insurance</p>
         </div>
-//         <div className="row">
-//             <div className="col-md-6 d-flex">
-//                 <div>
-//                     <h3>0 BUY TAX ON WEEKENDS</h3>
-//                     <p>Accumulate $BBT at 0 tax on weekends starting from 0 hours on Saturdays to 0 hours on Mondays</p>
-//                 </div>
-//             </div>
-//             <div className="col-md-6 d-flex">
-//                 <div>
-//                     <h3>25% SELL TAX ON WEEKENDS</h3>
-//                     <p>Holders enjoy 12% reflections when others sell on weekends and also strengthen LP with 3% going back to liquidity pool.</p>
-//                 </div>
-//             </div>
-//         </div>
+        {/*
+        <div className="row">
+            <div className="col-md-6 d-flex">
+                <div>
+                    <h3>0 BUY TAX ON WEEKENDS</h3>
+                    <p>Accumulate $BBT at 0 tax on weekends starting from 0 hours on Saturdays to 0 hours on Mondays</p>
+                </div>
+            </div>
+            <div className="col-md-6 d-flex">
+                <div>
+                    <h3>25% SELL TAX ON WEEKENDS</h3>
+                    <p>Holders enjoy 12% reflections when others sell on weekends and also strengthen LP with 3% going back to liquidity pool.</p>
+                </div>
+            </div>
+        </div>
+        */}
     </div>
   )
 }
